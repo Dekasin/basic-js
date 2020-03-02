be5febe9e41e44c930a3671ef175d71427d8ca1b
dekasins <dekasins@gmail.com>
2020-03-02 22:28:30 +0300

    feat: implement func createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(/* members */ arr) {
+ answer = false;
+  if (Array.isArray(arr) ==true) {
+ let team = [];
+ let j = 0;
+ for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) == 'string') {
+      for ( let k = 0; k < arr[i].length; k++) {
+          if (arr[i][k] != ' '){
+            team.push(arr[i][k].toUpperCase());
+            break;
+          }
+      }  
+        
+    }
+ }
+  answer = team.sort().join("");
+}
+  return answer;
 };
