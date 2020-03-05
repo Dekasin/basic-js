954ce27ac4f6ec4ed47edad482a50927e4c33b25
dekasins <dekasins@gmail.com>
2020-03-05 22:29:39 +0300

    fix: add trow Error

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,27 +1,30 @@
-module.exports = function transform(/* arr */ arr) {
-    try {
-        if (Array.isArray(arr)==false) throw Error;// I don't know what i do(((
-            }
-    catch (err){
-       arr = (err);
-    }
-    // remove line with error and write your code here
-   // if (Array.isArray(arr)) {
-    for (let i = 0; i < arr.length; i++) {
+module.exports = function transform(arr) {
+ 
+   if (Array.isArray(arr)) {
+
+       for (let i = 0; i < arr.length; i++) {
         switch(arr[i]) {
-            case "--discard-next" : arr.splice(i, 2);
-            break;
-            case "--discard-prev" : arr.splice(i-1, 2);
-            break;
-            case "--double-next" : arr[i] = arr[i+1];
-            break; 
-            case "--double-prev" : arr[i] = arr[i-1];
-            break;
+            case "--discard-next" : 
+            i == arr.length-1 ? arr.splice(i, 1) : arr.splice(i, 2);
+            i--;
+          break;
+            case "--discard-prev" : 
+            i == 0 ? arr.splice(i, 1):arr.splice(i-1, 2);
+            i--;
+          break;
+            case "--double-next" : 
+            i == arr.length-1 ? arr.splice(i, 1) : arr[i] = arr[i+1];
+            i--;
+          break; 
+            case "--double-prev" : 
+            i == 0 ? arr.splice(i, 1) : arr[i] = arr[i-1];
+            i--;
+          break;
         }
   
-    }
-// } else {
-//     arr="Error";
-//   }
+            }
+} else {
+    throw Error;
+  }
     return arr;
 };
