da5fccf299fd3c2212397d695d6e6e66e1918cd2
dekasins <dekasins@gmail.com>
2020-09-29 00:06:06 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,31 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  
+  //sampleActivity = parseFloat(sampleActivity);
+//  let typ = typeof(sampleActivity);
+//  if ( typ == 'undefined' || typ == 'object'  || typ == 'boolean' || typ == 'Null'||typ == 'number'){
+   
+//       return false;
+//  }
+if (typeof(sampleActivity) == "string"){
+ sampleActivity = parseFloat(sampleActivity);
+console.log( sampleActivity);
+  if (sampleActivity <= 15 && sampleActivity > 0) {
+    let t = 0,
+      k = 0,
+      N0 = MODERN_ACTIVITY,
+      N = sampleActivity,
+      T = HALF_LIFE_PERIOD;
+
+    k = 0.693 / T;
+
+    t = Math.log(N0 / N) / k;
+    return Math.ceil(t);
+    };
+  }
+  return false;
 };
