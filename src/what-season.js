a6ef65f8a5f8ec600dc18ef65dddea6a710d2ad6
dekasins <dekasins@gmail.com>
2020-10-02 13:36:26 +0300

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,30 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date ) {
+  
+  // let absense = "";
+  //   absense = date;
+
+  // if (absense == ""){
+  //   return 'Unable to determine the time of year!';
+  // }
+  
+  // if (isNaN(date)){
+  //   throw 'Error';
+  // }
+  
+  if (isNaN(date)){
+      return 'Unable to determine the time of year!';
+    }
+
+    let month = date.getMonth();
+    
+    if(month<2 || month >10){
+        return 'winter';
+    } else if (month<5){
+        return 'spring'; 
+    } else if (month<8){
+        return 'summer';
+    } else {
+        return ('autumn');
+    };
+  
 };
