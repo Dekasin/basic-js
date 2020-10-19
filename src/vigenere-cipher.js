db263e7b1b70955225a1fcddcd337504c1b71ef7
dekasins <dekasins@gmail.com>
2020-10-19 08:59:56 +0300

    test

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,11 +1,23 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
+    encrypt(message, key ) {
+        if (message == undefined || key == undefined){
+            throw new Error();
+        }
+        //throw 'Not implemented';
         // remove line with error and write your code here
+        //{\displaystyle c_{j}=(m_{j}+k_{j})\mod {n}}
+    let Message = message.toUpperCase().split('');
+    let Key = key.toUpperCase().split("");
+
+       let Number = Message.map(elem => {elem.charCode() });
+        
     }
 
-    decrypt() {
-        throw 'Not implemented';
+    decrypt(message, key ) {
+        if (message == undefined || key == undefined){
+            throw new Error();
+        }
+       // throw 'Not implemented';
         // remove line with error and write your code here
     }
 }
