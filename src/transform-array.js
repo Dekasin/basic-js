1aa0ec223f770624b13c92e298fa847be811ddd8
dekasins <dekasins@gmail.com>
2020-11-01 20:15:54 +0300

    fix: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,34 +1,40 @@
 module.exports = function transform(arr) {
- 
+ let arr2 = arr;
+ let skip = false;
    if (Array.isArray(arr)) {
+     
 
-       for (let i = 0; i < arr.length; i++) {
-        switch(arr[i]) {
+       for (let i = 0; i < arr2.length; i++) {
+        switch(arr2[i]) {
         
             case "--double-next" : 
-            i == arr.length-1 ? arr.splice(i, 1) : arr[i] = arr[i+1];
+            i == arr2.length-1 ? arr2.splice(i, 1) : arr2[i] = arr2[i+1];
             i--;
           break; 
             case "--double-prev" : 
-            i == 0 ? arr.splice(i, 1) : arr[i] = arr[i-1];
+           ( i == 0 || skip == true ) ? arr2.splice(i, 1) : arr2[i] = arr2[i-1];
             i--;
           break;
-        }
-      }
-        for (let i = 0; i < arr.length; i++) {
-          switch(arr[i]) {
-              case "--discard-next" : 
-              i == arr.length-1 ? arr.splice(i, 1) : arr.splice(i, 2);
+          case "--discard-next" : 
+              (i == arr2.length-1 ) ? arr2.splice(i, 1) : arr2.splice(i, 2);
               i--;
+              skip = true;
+
             break;
               case "--discard-prev" : 
-              i == 0 ? arr.splice(i, 1):arr.splice(i-1, 2);
+              (i == 0 || skip == true) ? arr2.splice(i, 1):arr2.splice(i-1, 2);
               i--;
-            }
-  
-            }
+              skip = true;
+              break;
+          
+          default :
+          skip == false
+
+        }
+      } 
+        
    } else {
     throw Error;
   }
-    return arr;
+    return arr2;
 };
