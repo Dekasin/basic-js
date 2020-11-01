3142b58062d2cf3dfac14710df99f5469034bd9c
dekasins <dekasins@gmail.com>
2020-11-01 14:00:14 +0300

    feat: realize viginere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,24 +1,54 @@
 class VigenereCipheringMachine {
-    encrypt(message, key ) {
-        if (message == undefined || key == undefined){
-            throw new Error();
-        }
-        //throw 'Not implemented';
-        // remove line with error and write your code here
-        //{\displaystyle c_{j}=(m_{j}+k_{j})\mod {n}}
-    let Message = message.toUpperCase().split('');
-    let Key = key.toUpperCase().split("");
-
-       let Number = Message.map(elem => {elem.charCode() });
-        
+    constructor(reverse) {
+        this.reverse = reverse
     }
 
-    decrypt(message, key ) {
-        if (message == undefined || key == undefined){
-            throw new Error();
+    encrypt(message, key) {
+        if (!message || !key) throw new Error()
+
+        let result ='';
+        let arr = [];
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+            for (let i = 65; i < 91; i++) {
+            arr.push(String.fromCharCode(i))
+            }
+
+        for (let i = 0, k  = 0; i < message.length; i++, k++) {
+            if (arr.includes(message[i])) {
+                let element = (arr.indexOf(message[i]) + arr.indexOf(key.charAt(k % key.length))) % arr.length
+                result += arr[element]
+            } else {
+               k--;
+                result += message[i];
+            }
         }
-       // throw 'Not implemented';
-        // remove line with error and write your code here
+        return (this.reverse === false) ? result.split('').reverse().join('') : result
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) throw new Error()
+
+        let result ='';
+        let arr = [];
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+
+        for (let i = 65; i < 91; i++) {
+            arr.push(String.fromCharCode(i));
+        }
+
+        for (let i = 0, k = 0; i < encryptedMessage.length; i++, k++) {
+            if (arr.includes(encryptedMessage[i])) {
+                let element = ((arr.indexOf(encryptedMessage[i]) + arr.length) - arr.indexOf(key.charAt(k % key.length))) % arr.length;
+                result += arr[element];
+            } else {
+                k--;
+                result += encryptedMessage[i];
+            }
+        }
+        return (this.reverse === false) ? result.split('').reverse().join('') : result
     }
 }
 
